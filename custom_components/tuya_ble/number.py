49163aa0ffa068f540cd2293e04c01adfbfc6bf8
stevew <stevewrightnz@hotmail.com>
2024-04-19 11:59:33 +1200

    Update number.py

--- custom_components/tuya_ble/number.py
+++ custom_components/tuya_ble/number.py
@@ -15,7 +15,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     CONCENTRATION_PARTS_PER_MILLION,
     PERCENTAGE,
-    VOLUME_MILLILITERS,
+    UnitOfVolume.MILLILITERS,
     TIME_MINUTES,
     UnitOfTime,
     UnitOfVolume,
@@ -405,7 +405,7 @@ mapping: dict[str, TuyaBLECategoryNumberMapping] = {
                         device_class=NumberDeviceClass.WATER,
                         native_max_value=5000,
                         native_min_value=0,
-                        native_unit_of_measurement=VOLUME_MILLILITERS,
+                        native_unit_of_measurement=UnitOfVolume.MILLILITERS,
                         native_step=1,
                         entity_category=EntityCategory.CONFIG,
                     ),
